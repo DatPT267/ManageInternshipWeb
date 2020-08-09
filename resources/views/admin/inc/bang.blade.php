386c8011f05376a974543cb960b5632e97638eb1
thanhtan16cntt3 <dv.nttan98@gmail.com>
2020-08-09 23:18:31 +0700

    edit col action in bang.blade.php

--- resources/views/admin/inc/bang.blade.php
+++ resources/views/admin/inc/bang.blade.php
@@ -4,8 +4,11 @@
             <th>ID</th>
             <th>Tên</th>
             <th>Status</th>
-            <th>Delete</th>
-            <th>Edit</th>
+            <th>Status</th>
+            <th>Status</th>
+            <th>Status</th>
+            <th>Status</th>
+            <th>Action</th>
         </tr>
     </thead>
     <tbody>
@@ -13,29 +16,53 @@
             <td>1</td>
             <td>name1</td>
             <td>status</td>
-            <td class="center"><i class="fas fa-trash-alt"></i><a href="#"> Delete</a></td>
-            <td class="center"><i class="fas fa-edit"></i> <a href="#">Edit</a></td>
+            <td>status</td>
+            <td>status</td>
+            <td>status</td>
+            <td>status</td>
+            <td class="center">
+                <a href="#"><i class="fas fa-trash-alt" ></i> Delete</a> |
+                <a href="#"><i class="fas fa-edit"></i> Edit</a>
+            </td>
         </tr>
         <tr class="odd gradeX" align="center">
             <td>1</td>
             <td>name2</td>
             <td>status</td>
-            <td class="center"><i class="fas fa-trash-alt"></i><a href="#"> Delete</a></td>
-            <td class="center"><i class="fas fa-edit"></i> <a href="#">Edit</a></td>
+            <td>Image</td>
+            <td>status</td>
+            <td>status</td>
+            <td>status</td>
+            <td class="center">
+                <a href="#"><i class="fas fa-trash-alt" ></i> Delete</a> |
+                <a href="#"><i class="fas fa-edit"></i> Edit</a>
+            </td>
         </tr>
         <tr class="odd gradeX" align="center">
             <td>1</td>
             <td>name3</td>
             <td>status</td>
-            <td class="center"><i class="fas fa-trash-alt"></i><a href="#"> Delete</a></td>
-            <td class="center"><i class="fas fa-edit"></i><a href="#">Edit</a></td>
+            <td>status</td>
+            <td>status</td>
+            <td>status</td>
+            <td>status</td>
+            <td class="center">
+                <a href="#"><i class="fas fa-trash-alt" ></i> Delete</a> |
+                <a href="#"><i class="fas fa-edit"></i> Edit</a>
+            </td>
         </tr>
         <tr class="odd gradeX" align="center">
             <td>1</td>
             <td>name3</td>
             <td>status</td>
-            <td class="center"><i class="fas fa-trash-alt"></i><a href="#"> Delete</a></td>
-            <td class="center"><i class="fas fa-edit"></i> <a href="#">Edit</a></td>
+            <td>status</td>
+            <td>status</td>
+            <td>status</td>
+            <td>status</td>
+            <td class="center">
+                <a href="#"><i class="fas fa-trash-alt" ></i> Delete</a> |
+                <a href="#"><i class="fas fa-edit"></i> Edit</a>
+            </td>
         </tr>
     </tbody>
 </table>

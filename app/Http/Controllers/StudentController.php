e32d8b8d9d393ba74d9db266ef9e0f0304be9fc4
thanhtan16cntt3 <54431533+thanhtan16cntt3@users.noreply.github.com>
2020-08-15 00:31:21 +0700

    issues15 done

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Schedule;
+use App\User;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use DateTime;
+
+class StudentController extends Controller
+{
+    public function viewSchedule($id)
+    {
+        $student = User::find($id);
+        $schedules = Schedule::where('user_id', $id)->get();
+        // $ngay = Carbon::parse($schedules->date)->format('l');
+        // $gio = Carbon::parse($schedules->date)->format('H');
+        // $phut = Carbon::parse($schedules->date)->format('i');
+        $arrayDayOfWeek = array();
+        foreach ($schedules as $value) {
+            $now = Carbon::now();
+            $time = Carbon::parse($value->date);
+            if($time->isCurrentMonth() && $time->weekNumberInMonth == $now->weekNumberInMonth){
+                $day = Carbon::parse($value->date)->englishDayOfWeek;
+                $arrayDayOfWeek[$day] = array('session'=>$value->session);
+            }
+        }
+        // dd($arrayDayOfWeek);
+        $i=0;
+        return view('admin.pages.manageStudents.show-regSchedule', ['studentName'=>$student->name, 'arrayDayOfWeek'=>$arrayDayOfWeek, 'index'=>$i]);
+    }
+}

7ede876aeae52087577829e4b182e22284ad2193
DatPT267 <ptdat.18it5@sict.udn.vn>
2020-10-15 22:23:06 +0700

    review branch

--- resources/views/admin/authentication/login.blade.php
+++ resources/views/admin/authentication/login.blade.php
@@ -18,13 +18,13 @@
         @endif
         </div>
         <h1>Vui Lòng Đăng Nhập</h1>
-        <form role="form" action="../admin/login" method="POST">
+            <form role="form" action="{{ route('login')}}" method="POST">
             <input type="hidden" name="_token" value="{{csrf_token()}}">
             <p>Tài Khoản</p>
             <input type="text" name="account" placeholder="">
             <p>Mật Khẩu</p>
             <input type="password" name="password" placeholder="">
-            <input type="submit" name="" value="Login">
+            <input type="submit" name="" value="Đăng Nhập">                                                                                                                  
             <a href="{{ route('losspassword') }}">Quên Mật Khẩu?</a><br>   
         </form>  
     </div>

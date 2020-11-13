7ddc2a27a99d15379db88292439b841de18ef029
DatPT267 <ptdat.18it5@sict.udn.vn>
2020-11-13 16:36:28 +0700

    fix

--- app/Http/Controllers/InternshipclassController.php
+++ app/Http/Controllers/InternshipclassController.php
@@ -113,7 +113,7 @@ class InternshipclassController extends Controller
           [
 
               'name' =>'required|unique:Internshipclass,name',
-              'start_day'=>'required|date',
+              'start_day'=>'required|date|before:end_date',
               'end_day'=>'required|date',
               
 
@@ -123,14 +123,11 @@ class InternshipclassController extends Controller
               'name.required' =>'Bạn chưa nhập tên đợt thực tập',
               'start_day.required' => 'Bạn chưa nhập ngày bắt đầu',
               'end_day.required' => 'Bạn chưa nhập ngày kết thúc',
+              'start_day.before' => 'Ngày kết thúc không phù hợp',
             
 
           ]);
-      $start_day = $request->start_day;
-      $end_day = $request->end_day;
-      if (strtotime($start_day) > strtotime($end_day)){
-        return back()->with('thongbao', 'Ngày kết thúc không phù hợp');
-      }
+   
       for( $i=0 ; $i < 25 ; $i++){
           $member[$i] = $i;
       }
@@ -155,7 +152,7 @@ class InternshipclassController extends Controller
             [ 
 
                 'name' =>'required',
-                'start_day'=>'required|date',
+                'start_day'=>'required|date|before:end_date',
                 'end_day'=>'required|date',
 
             ],
@@ -163,14 +160,15 @@ class InternshipclassController extends Controller
                 'name.required' =>'Bạn chưa nhập tên đợt thực tập',
                 'start_day.required' => 'Bạn chưa nhập ngày bắt đầu',
                 'end_day.required' => 'Bạn chưa nhập ngày kết thúc',
+                'start_day.before' => 'Ngày kết thúc không phù hợp',
 
             ]);
 
-        $start_day = $request->start_day;
-        $end_day = $request->end_day;
-        if (strtotime($start_day) > strtotime($end_day)){
-          return back()->with('thongbao', 'Ngày kết thúc dự kiến không phù hợp');
-        }
+        // $start_day = $request->start_day;
+        // $end_day = $request->end_day;
+        // if (strtotime($start_day) > strtotime($end_day)){
+        //   return back()->with('thongbao', 'Ngày kết thúc dự kiến không phù hợp');
+        // }
         $internshipclass = Internshipclass::find($id);
         $internshipclass->name = $request->name;
         $internshipclass->end_day = $request->end_day;
